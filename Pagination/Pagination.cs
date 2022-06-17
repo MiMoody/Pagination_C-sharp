3b7a4acb83f8ba471d3f38cf3813dd887d749536
Artem <svev2369@gmail.com>
2022-06-17 17:29:20 +0700

    change index page

---
+++ Pagination/Pagination.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagination
+{
+    class Pagination<T>
+    {
+        protected int totalPages = 0;
+        protected int currentIndexPage = 1;
+        protected int pageSize = 10;
+        protected List<T> currentPage;
+        private List<T> data;
+        public int TotalPages { get { return this.totalPages; } }
+        public int CurrentIndexPage { get { return this.currentIndexPage; } }
+        public List<T> CurrentPage { get { return this.currentPage; } }
+
+        private void UpdateData()
+        {
+            if (this.data != null)
+            {
+                if (this.pageSize > this.data.Count()) this.pageSize = this.data.Count();
+                CalculateTotalPages();
+                GetCurrentRecords(1);
+            }
+        }
+        public List<T> GetData(int page)
+        {
+            // Функция служит для получения даннх для определенной страницы
+
+            GetCurrentRecords(page);
+            return this.currentPage;
+        }
+        public void SetData(List<T> data)
+        {
+            // Метод служаит для установки list, по которому необходимо произвести пагинацию
+            this.data = data;
+            UpdateData();
+        }
+
+        public void SetPageSize(int size)
+        {
+            this.pageSize = size;
+            UpdateData();
+        }
+
+        private void CalculateTotalPages()
+        {
+            // Метод пересчитывает количество страниц
+            int rowCount;
+            rowCount = Convert.ToInt32(this.data.Count());
+            this.totalPages = rowCount / this.pageSize;
+            if (rowCount % this.pageSize > 0)
+                this.totalPages += 1;
+        }
+
+        private void GetCurrentRecords(int page)
+        {
+            // Метод устанавливает содержание страницы по нужному номеру
+            if (page <= 0 || page > TotalPages)
+            {
+                Console.WriteLine("Bad index...");
+                return;
+            }
+            currentIndexPage = page;
+
+            if (page == 1) this.currentPage = this.data.Take(this.pageSize).ToList();
+            else
+            {
+                int previousPageOffSet = (page - 1) * this.pageSize;
+                List<T> excludeData = this.data.Take(previousPageOffSet).ToList();
+                this.currentPage = this.data.Where(p => !excludeData.Contains(p)).Take(this.pageSize).ToList();
+            }
+        }
+
+        public List<T> NextPage()
+        {
+            if (this.currentIndexPage < TotalPages)
+            {
+                this.currentIndexPage++;
+                GetCurrentRecords(this.currentIndexPage);
+            }
+            return this.currentPage;
+        }
+        public List<T> PrevPage()
+        {
+            if (this.currentIndexPage > 1)
+            {
+                this.currentIndexPage--;
+                GetCurrentRecords(this.currentIndexPage);
+            }
+            return this.currentPage;
+        }
+
+
+
+    }
+}
--- Pagination/Program.cs
+++ Pagination/Program.cs
@@ -2,47 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pagination
 {
+    
     class Program
     {
-        static void Main(string[] args)
+        public static void PrintTime()
         {
+            DateTime dateTime = new DateTime(2022, 06, 14, 13, 45, 0);
+            while (true)
+            {
+                var dt = dateTime - DateTime.Now;
+                var timeSpan = new TimeSpan(dt.Days, dt.Hours, dt.Minutes, dt.Seconds);
+                Console.WriteLine(timeSpan.Days + "д. " + timeSpan.Hours + " ч. " + timeSpan.Minutes + " м. " + timeSpan.Seconds + " с.");
+                Thread.Sleep(1000);
+            }
+        }
+
+         static void print()
+        {   
+            while (true)
+            {
+                Console.WriteLine("Nice...");
+                Thread.Sleep(1000);
+            }
+        }
+
+        async static Task P()
+        {
+            await Task.Run(print);
+        } 
+
+        async static Task Main(string[] args)
+        {
+
+            //Thread thread = new Thread(new ThreadStart(PrintTime));
+            //thread.Start();
+            //await P();
+            //Task task = new Task(print);
+            //task.Start();
             Pagination<Client> pagination = new Pagination<Client>();
             var clients = SetClients(1000);
-            pagination.SetPageSize(100);
+            pagination.SetPageSize(10);
             pagination.SetData(clients);
             clients = pagination.CurrentPage;
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             clients = pagination.PrevPage();
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             clients = pagination.NextPage();
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             clients = pagination.PrevPage();
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             clients = pagination.NextPage();
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             Console.ReadLine();
-
             pagination.SetPageSize(10);
             clients = pagination.GetData(2);
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             clients = pagination.GetData(8);
-            Console.WriteLine("\n Новая страница \n");
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
-            clients = pagination.GetData(20);
-            Console.WriteLine("\n Новая страница \n");
+            clients = pagination.GetData(1);
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
-            clients = pagination.GetData(200);
-            Console.WriteLine("\n Новая страница \n");
+            clients = pagination.GetData(5);
+            Console.WriteLine($"\n Новая страница: {pagination.CurrentIndexPage} из {pagination.TotalPages} \n");
             PrintClients(clients);
             Console.ReadLine();
 
@@ -84,94 +117,5 @@ namespace Pagination
         }
     }
 
-    class Pagination<T>
-    {
-        protected int totalPages = 0;
-        protected int currentIndexPage = 1;
-        protected int pageSize = 10;
-        protected List<T> currentPage;
-        private List<T> data;
-        public int TotalPages { get { return this.totalPages; } }
-        public int CurrentIndexPage { get { return this.currentIndexPage; } }
-        public List<T> CurrentPage { get { return this.currentPage; } }
-
-        private void UpdateData()
-        {
-            if (this.data != null)
-            {
-               if (this.pageSize > this.data.Count()) this.pageSize = this.data.Count();
-                CalculateTotalPages();
-                GetCurrentRecords(1);
-            }
-        }
-        public List<T> GetData(int page)
-        {
-            // Функция служит для получения даннх для определенной страницы
-
-            GetCurrentRecords(page);
-            return this.currentPage;
-        }
-        public void SetData(List<T> data)
-        {
-            // Метод служаит для установки list, по которому необходимо произвести пагинацию
-            this.data = data;
-            UpdateData();
-        }
-
-        public void SetPageSize(int size)
-        {
-            this.pageSize = size;
-            UpdateData();
-        }
-
-        private void CalculateTotalPages()
-        {
-            // Метод пересчитывает количество страниц
-            int rowCount;
-            rowCount = Convert.ToInt32(this.data.Count());
-            this.totalPages = rowCount / this.pageSize;
-            if (rowCount % this.pageSize > 0)
-                this.totalPages += 1;
-        }
-
-        private void GetCurrentRecords(int page)
-        {
-            // Метод устанавливает содержание страницы по нужному номеру
-            if (page <= 0 || page > TotalPages)
-            {
-                Console.WriteLine("Bad index...");
-                return;
-            }
-
-            if (page == 1) this.currentPage = this.data.Take(this.pageSize).ToList();
-            else
-            {
-                int previousPageOffSet = (page - 1) * this.pageSize;
-                List<T> excludeData = this.data.Take(previousPageOffSet).ToList();
-                this.currentPage = this.data.Where(p => !excludeData.Contains(p)).Take(this.pageSize).ToList();
-            }
-        }
-
-        public List<T> NextPage()
-        {
-            if (this.currentIndexPage < TotalPages)
-            {
-                this.currentIndexPage++;
-                GetCurrentRecords(this.currentIndexPage);
-            }
-            return this.currentPage;
-        }
-        public List<T> PrevPage()
-        {
-            if (this.currentIndexPage > 1)
-            {
-                this.currentIndexPage--;
-                GetCurrentRecords(this.currentIndexPage);
-            }
-            return this.currentPage;
-        }
-
-
-
-    }
+    
 }
